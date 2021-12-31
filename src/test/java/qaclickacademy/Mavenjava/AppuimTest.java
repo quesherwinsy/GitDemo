58c8410444426c6ae751de6b4d3416392a58df36
quesherwinsy <quesherwinsy@gmail.com>
2021-12-31 21:56:32 +0800

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/AppuimTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppuimTest {
+	
+	@Test
+	public void NativeAppAndroid() {
+		System.out.println("NativeAppAndroid");
+	}
+
+	@Test
+	public void IOSApps() {
+		System.out.println("IOSApps");
+	}
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/RESRAPITest.java
@@ -0,0 +1,16 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RESRAPITest {
+	
+	@Test
+	public void postJira() {
+		System.out.println("postJira");
+	}
+
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+	}
+}
